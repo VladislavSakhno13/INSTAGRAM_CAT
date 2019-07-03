3cc8f234c164a57290c99ec82cd707f415c304bd
VladislavSakhno13 <sahno.99@inbox.ru>
2019-07-03 16:42:12 +0300

    newjs

--- cat_js.js
+++ cat_js.js
@@ -1,3 +1,17 @@
+var xhr = new XMLHttpRequest();
+
+      xhr.open('GET', 'catsstore.json', false);
+      xhr.send();
+
+      if (xhr.status != 200) {
+        // обработать ошибку
+        alert('Ошибка ' + xhr.status + ': ' + xhr.statusText);
+      } else {
+        // вывести результат
+        alert(xhr.responseText);
+      }
+    
+
 
 
 const cats = [
