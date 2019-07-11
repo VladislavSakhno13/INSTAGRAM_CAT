b1a32cab93b9df188a4b03f6e4973893393d925d
VladislavSakhno13 <sahno.99@inbox.ru>
2019-07-11 18:21:59 +0300

    ColorButtonRemoweButtonInFavorite

--- cat_js.js
+++ cat_js.js
@@ -65,64 +65,73 @@ function createfavoritCat(cat) {
 }
 
 
-function createCatHtml(cat,bool) {
+function createCatHtml(cat,isFavorites) {
     const div = document.createElement('div');
-    if(bool == true){
+    if(isFavorites == true){
     div.id = 'cat_' + cat.id;
     }
     else div.id = 'favor_' + cat.id;
+    
 
     const p = document.createElement('p');
+    div.appendChild(p);
 
     const img = document.createElement('img');
     img.src = cat.url;
+    p.appendChild(img);
 
     const like = document.createElement('input');
-
     like.type = "button";
     like.value = "Like";
     like.id = "like";
+    div.appendChild(like);
 
     const span = document.createElement('span');
     span.id = "count";
+    div.appendChild(span);
 
+    if(isFavorites == true){
     const input = document.createElement('input');
     input.id = "text";
+    div.appendChild(input);
 
     const save = document.createElement('input');
     save.type = "button";
     save.value = "Save";
     save.id = "save";
+    div.appendChild(save);
+
+    save.onclick = function() {
+        var e = input.value;
+        cat.title = e;
+        updateCatHtml(this.parentElement, cat);
+    }
+    }
 
     const nameCat = document.createElement('span');
     nameCat.id = "nameImg";
+    div.appendChild(nameCat);
 
+    if(isFavorites == true){
     const favoritImg = document.createElement('input');
     favoritImg.type = "button";
     favoritImg.id = 'favoritIMG_' + cat.id;
     favoritImg.value = "favorits";
     favoritImg.className = "getCat";
-
-    div.appendChild(p);
-    p.appendChild(img);
-    div.appendChild(like);
-    div.appendChild(span);
-    div.appendChild(input);
-    div.appendChild(save);
-    div.appendChild(nameCat);
     div.appendChild(favoritImg);
+    favoritImg.onclick = function() {addToFaforitesList(favoritImg)};
+    }
+
+    
 
     like.onclick = function () {
         cat.likes++;
         updateCatHtml(this.parentElement, cat);
         like.style.background = 'red';
     }
-    save.onclick = function () {
-        var e = input.value;
-        cat.title = e;
-        updateCatHtml(this.parentElement, cat);
-    }
-    favoritImg.onclick =  function() {
+     
+      function addToFaforitesList(favoritImg) {
+        favoritImg.style.background = 'red';
         var index;
         for(var i = 0; i< cats.length;i++){
             if(cat == catsfavorites[i]){
@@ -130,15 +139,13 @@ function createCatHtml(cat,bool) {
             }
             
         }
-        let result =  catsfavorites.find(item => item.id == cat.id );
-        if (result){
+        if (index != undefined){
             console.log('cat found');
             var perent = document.getElementById('favorites');
             var child = document.getElementById('favor_' + cat.id);
             perent.removeChild(child);
-        
-       
             catsfavorites.splice(index,1);
+            favoritImg.style.background = null;
             return;
         }
         catsfavorites.push(cat);
